dc4cd3865a3c24b59bb6462f10b5231f4b2dab9a
avisheklive@gmail.com <avisheklive@gmail.com>
2018-12-08 13:03:39 +0530

    first comm

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author avi
+ */
+public class NewClass {
+    
+    public static void main (String Args[]){
+    
+        System.out.println("test class");
+    }
+    
+}
